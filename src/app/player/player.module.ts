6fa4db1ed4d178796b373ffbdedc74741bf54b9e
jodu07 <jhonnat.dussan@gmail.com>
2022-04-06 01:18:53 -0500

    styles login

--- src/app/player/player.module.ts
+++ src/app/player/player.module.ts
@@ -4,16 +4,12 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 
-
 import { PlayerPageComponent } from './pages/player-page/player-page.component';
 import { PlayerTableComponent } from './components/player-table/player-table.component';
 import { PlayerCardComponent } from './components/player-card/player-card.component';
 import { ForPlayerComponent } from './pages/for-player/for-player.component';
 import { PruebasComponent } from './pages/pruebas/pruebas.component';
 import { RegisterPlayerComponent } from './pages/register-player/register-player.component';
-
-
-
 
 
 @NgModule({
